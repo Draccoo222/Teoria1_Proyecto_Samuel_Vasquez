d176e67e6aabb9c9935c66cba70c2c8194517988
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2026-09-23 00:49:59 -0600

    correccion backend

--- backend/server.ts
+++ backend/server.ts
@@ -196,15 +196,12 @@ app.get('/api/presupuestos/:id', async (req, res) => {
 app.get('/api/presupuestos/:id/json', async (req, res) => {
     try {
         const conn = await ibmdb.open(connStr);
-        const data = await conn.query(`CALL sp_obtener_presupuesto_json(${req.params.id}, ?)`, [null]);
+        const data = await conn.query(`CALL sp_obtener_presupuesto_json(${req.params.id})`);
         await conn.close();
+   
+        const stringJson =  Object.values(data[0])[0];
 
-        let resultadoStr = Array.isArray(data) ? data[0] : data;
-        if (typeof resultadoStr === 'object' && resultadoStr !== null) {
-            resultadoStr = Object.values(resultadoStr)[0];
-        }
-
-        res.status(200).json(JSON.parse(String(resultadoStr)));
+        res.status(200).json(JSON.parse(String(stringJson)));
     } catch (e: any) { res.status(500).json({ error: e.message }); }
 });
 
